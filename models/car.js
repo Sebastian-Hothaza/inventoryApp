9f1754ed3d916f372e6b5924f727d4b52e39b8fd
Sebastian Hothaza <sebastianhothaza@gmail.com>
2024-02-17 18:21:43 -0500

    Added inventory tracking piece, oops!

--- models/car.js
+++ models/car.js
@@ -6,6 +6,7 @@ const carSchema = new Schema({
     make: {type: String, required: true, maxLength: 100 },
     model: {type: String, required: true, maxLength: 100 },
     year: {type: Number, required: true },
+    stock: {type: Number},
     category: {type: Schema.Types.ObjectId, ref: "Category", required: true},
 });
 
