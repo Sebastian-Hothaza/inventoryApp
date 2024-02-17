5236e55c11486e0e497c90bdc1bcff76b2189b8d
Sebastian Hothaza <sebastianhothaza@gmail.com>
2024-02-17 16:08:34 -0500

    Added Routing

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,60 @@
 const express = require('express');
 const router = express.Router();
 
+// Require controller modules.
+const carController = require('../controllers/carController');
+const categoryController = require('../controllers/categoryController')
+
+
+// CAR ROUTES
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', carController.index);
+
+// GET request for creating a car
+router.get('/car/create', carController.carCreate_get);
+// POST request for creating a car
+router.post('/car/create', carController.carCreate_post);
+
+// GET request for viewing a car
+router.get('/car/:id', carController.carDetail);
+// GET request for viewing all cars
+router.get('/cars', carController.carsDetail);
+router.get('/car', (req, res, next) => res.redirect('/cars'));
+
+// GET request for updating a car
+router.get('/car/update/:id', carController.carUpdate_get);
+// POST request for updating a car
+router.post('/car/update/:id', carController.carUpdate_post);
+
+// GET request for deleting a car
+router.get('/car/delete/:id', carController.carDelete_get);
+// POST request for updating a car
+router.post('/car/delete/:id', carController.carDelete_post);
+
+
+// CATEGORY ROUTES
+
+
+// GET request for creating a category
+router.get('/category/create', categoryController.categoryCreate_get);
+// POST request for creating a category
+router.post('/category/create', categoryController.categoryCreate_post);
+
+// GET request for viewing a category
+router.get('/category/:id', categoryController.categoryDetail);
+// GET request for viewing all category
+router.get('/categories', categoryController.categoriesDetail);
+router.get('/category', (req, res, next) => res.redirect('/categories'));
+
+// GET request for updating a category
+router.get('/category/update/:id', categoryController.categoryUpdate_get);
+// POST request for updating a category
+router.post('/category/update/:id', categoryController.categoryUpdate_post);
+
+// GET request for deleting a category
+router.get('/category/delete/:id', categoryController.categoryDelete_get);
+// POST request for deleting a category
+router.post('/category/delete/:id', categoryController.categoryDelete_post);
 
 module.exports = router;
