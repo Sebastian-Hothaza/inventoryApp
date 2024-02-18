b1d70c9f221519d55270bbace9ba62fd77bf427d
Sebastian Hothaza <sebastianhothaza@gmail.com>
2024-02-17 20:55:57 -0500

    Added support for create and update

--- controllers/carController.js
+++ controllers/carController.js
@@ -2,6 +2,7 @@ const Car = require('../models/car');
 const Category = require('../models/category');
 
 const asyncHandler = require("express-async-handler");
+const { body, validationResult } = require("express-validator");
 
 // HOMEPAGE
 exports.index = asyncHandler(async (req, res, next) => {
@@ -12,11 +13,53 @@ exports.index = asyncHandler(async (req, res, next) => {
 
 // CREATE
 exports.carCreate_get = asyncHandler(async (req, res, next) => {
-    res.send("carCreate_get not yet implemented!")
+    const allCategories = await Category.find().exec();
+    res.render("car_form", {title: "Create a new Car", allCategories});
 })
-exports.carCreate_post = asyncHandler(async (req, res, next) => {
-    res.send("carCreate_post not yet implemented!")
-})
+
+exports.carCreate_post = [
+    // Validate and sanitize fields
+    body("year", 'Year must be in range of 1900-2100').isInt({min: 1900, max: 2100}).escape(),
+    body("make", "Make must not be empty").isLength({min: 1}).escape(),
+    body("model", "Model must not be empty").isLength({min: 1}).escape(),
+    body("stock", "Stock must be in range of 0-100").isInt({min:0, max:100}).escape(),
+    body("category.*").escape(),
+
+    // Process the request
+    asyncHandler(async (req, res, next) => {
+        // Extract the validation errors from a request.
+        const errors = validationResult(req);
+
+        // Create new car object
+        const car = new Car({
+            year: req.body.year,
+            make: req.body.make,
+            model: req.body.model,
+            stock: req.body.stock,
+            category: req.body.category
+        });
+
+        if(!errors.isEmpty()){
+            // There are errors. Render form again with sanitized values/errors messages.
+            const allCategories = await Category.find().exec();
+            res.render("car_form", {title: "Create a new Car", car, allCategories, errors:errors.array()});
+
+        }else{
+            // Form data is valid!
+
+            // Check if we already have the same car in the DB, if we do, then just append the inventory to it
+            const carExists = await Car.findOne({year: car.year, make: car.make, model: req.body.model, category: car.category});
+            if (carExists){
+                car._id=carExists._id; //Augment the created object to duplicate the existing objects ID field
+                const updatedCar = await Car.findByIdAndUpdate(car._id, car, {});
+                res.redirect(updatedCar.url);
+            }else{
+                await car.save();
+                res.redirect(car.url);
+            }
+        }
+    })
+]
 
 
 // READ
@@ -32,11 +75,52 @@ exports.carsDetail = asyncHandler(async (req, res, next) => {
 
 // UPDATE
 exports.carUpdate_get = asyncHandler(async (req, res, next) => {
-    res.send("carUpdate_get not yet implemented!")
+    const allCategories = await Category.find().exec();
+    const car = await Car.findById(req.params.id).exec();
+
+    if (car === null){
+        const err = new Error("Car not found");
+        err.status = 404;
+        return next(err);
+    }
+
+    res.render("car_form", {title: "Edit Car", car, allCategories})
 })
-exports.carUpdate_post = asyncHandler(async (req, res, next) => {
-    res.send("carUpdate_post not yet implemented!")
-})
+exports.carUpdate_post = [
+        // Validate and sanitize fields
+        body("year", 'Year must be in range of 1900-2100').isInt({min: 1900, max: 2100}).escape(),
+        body("make", "Make must not be empty").isLength({min: 1}).escape(),
+        body("model", "Model must not be empty").isLength({min: 1}).escape(),
+        body("stock", "Stock must be in range of 0-100").isInt({min:0, max:100}).escape(),
+        body("category.*").escape(),
+    
+        // Process the request
+        asyncHandler(async (req, res, next) => {
+            // Extract the validation errors from a request.
+            const errors = validationResult(req);
+    
+            // Create new car object
+            const car = new Car({
+                year: req.body.year,
+                make: req.body.make,
+                model: req.body.model,
+                stock: req.body.stock,
+                category: req.body.category,
+                _id: req.params.id
+            });
+    
+            if(!errors.isEmpty()){
+                // There are errors. Render form again with sanitized values/errors messages.
+                const allCategories = await Category.find().exec();
+                res.render("car_form", {title: "Edit Car", car, allCategories, errors:errors.array()});
+    
+            }else{
+                // Form data is valid! Update the recod
+                await Car.findByIdAndUpdate(car._id, car, {});
+                res.redirect(car.url);
+            }
+        })
+]
 
 
 // DELETE
--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -6,16 +6,48 @@ const { body, validationResult } = require("express-validator");
 
 // CREATE
 exports.categoryCreate_get = asyncHandler(async (req, res, next) => {
-    res.send("categoryCreate_get not yet implemented!")
+    res.render("category_form", {title: "Create a new Category"})
 })
-exports.categoryCreate_post = asyncHandler(async (req, res, next) => {
-    res.send("categoryCreate_post not yet implemented!")
-})
+exports.categoryCreate_post = [
+        // Validate and sanitize fields
+        body("name", "Name must not be empty").isLength({min: 1}).escape(),
+        body("commercial.*").escape(),
+        
+        // Process the request
+        asyncHandler(async (req, res, next) => {
+            // Extract the validation errors from a request.
+            const errors = validationResult(req);
+    
+   
+            // Create new category object
+            const isCommercial = req.body.commercial===undefined? false:true; 
+            const category = new Category({
+                name: req.body.name,
+                commercial: isCommercial,
+            });
+    
+            if(!errors.isEmpty()){
+                // There are errors. Render form again with sanitized values/errors messages.
+                res.render("category_form", {title: "Create a new Category", category, errors:errors.array()});
+    
+            }else{
+                // Form data is valid!
+    
+                // Check if we already have the same category in the DB
+                const categoryExists = await Category.findOne({name: category.name, commercial: category.commercial});
+                if (categoryExists){
+                    res.redirect(categoryExists.url);
+                }else{
+                    await category.save();
+                    res.redirect(category.url);
+                }
+            }
+        })
+]
 
 // READ
 exports.categoryDetail = asyncHandler(async (req, res, next) => {
     const [category, cars] = await Promise.all([Category.findById(req.params.id).exec(), Car.find({category: req.params.id})]);
-
     res.render("category_detail", {title: "Details about this Category", category, cars})
 })
 
@@ -26,11 +58,39 @@ exports.categoriesDetail = asyncHandler(async (req, res, next) => {
 
 // UPDATE
 exports.categoryUpdate_get = asyncHandler(async (req, res, next) => {
-    res.send("categoryUpdate_get not yet implemented!")
+    const category = await Category.findById(req.params.id).exec();
+    res.render("category_form", {title: "Edit a Category", category})
 })
-exports.categoryUpdate_post = asyncHandler(async (req, res, next) => {
-    res.send("categoryUpdate_post not yet implemented!")
-})
+exports.categoryUpdate_post = [
+    // Validate and sanitize fields
+    body("name", "Name must not be empty").isLength({min: 1}).escape(),
+    body("commercial.*").escape(),
+    
+    // Process the request
+    asyncHandler(async (req, res, next) => {
+        // Extract the validation errors from a request.
+        const errors = validationResult(req);
+
+
+        // Create new category object
+        const isCommercial = req.body.commercial===undefined? false:true; 
+        const category = new Category({
+            name: req.body.name,
+            commercial: isCommercial,
+            _id: req.params.id
+        });
+
+        if(!errors.isEmpty()){
+            // There are errors. Render form again with sanitized values/errors messages.
+            res.render("category_form", {title: "Edit a Category", category, errors:errors.array()});
+
+        }else{
+            // Form data is valid! Update the member
+            const updatedCategory = await Category.findByIdAndUpdate(req.params.id, category, {})
+            res.redirect(updatedCategory.url);
+        }
+    })
+]
 
 // DELETE
 exports.categoryDelete_get = asyncHandler(async (req, res, next) => {
